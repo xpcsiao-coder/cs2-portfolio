10fd95ee1f2e9ca81303abc46db215a343731315
xpcsiao-coder <xpcsiao@brc.pshs.edu.ph>
2026-09-07 20:12:03 +0800

    Implement score validation and feedback system
    Added score validation and feedback messages based on student score.

--- q1/score_checker.py
+++ q1/score_checker.py
@@ -1,1 +1,19 @@
+#Get the score of the student.
+score = int(input("What is your score? "))
 
+#Identify whether his/her score is Invalid.
+if score < 0 or score > 100: 
+  print("Invalid, try again.")
+
+#Now identify whether his/her score is Outstanding, Very Satisfactory, Satisfactory, or Needs Improvement.
+elif score >= 90: 
+  print("Outstanding, Very good.")
+
+elif score >= 80:
+  print ("Very satisfactory, Great job.")
+
+elif score >= 75: 
+  print("Satisfactory, Good job.")
+
+else: 
+  print("Needs improvement."
